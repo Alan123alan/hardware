91a1f94c0eb2c62fc0aa64dce6afbab372d5206c
Alan123alan <alancastellanos2@hotmail.com>
2023-06-17 09:03:56 -0600

    Corrected implementation of 16 bit or logic gate, it now operates on two sixteen bit registers

--- or16.v
+++ or16.v
@@ -1,14 +1,31 @@
-module sixteen_bits_or;
-//declare circuit inputs and outputs
-reg [15:0] in;
-wire out;
-//modelling the circuit
-or(out,in[15],in[14],in[13],in[12],in[11],in[10],in[9],in[8],in[7],in[6],in[5],in[4],in[3],in[2],in[1],in[0]);
+module Or16;
+//declaring circuit inputs and outputs
+reg [15:0] in0;
+reg [15:0] in1;
+wire [15:0] out;
+//modelling circuit
+or(out[15],in0[15],in1[15]);
+or(out[14],in0[14],in1[14]);
+or(out[13],in0[13],in1[13]);
+or(out[12],in0[12],in1[12]);
+or(out[11],in0[11],in1[11]);
+or(out[10],in0[10],in1[10]);
+or(out[9],in0[9],in1[9]);
+or(out[8],in0[8],in1[8]);
+or(out[7],in0[7],in1[7]);
+or(out[6],in0[6],in1[6]);
+or(out[5],in0[5],in1[5]);
+or(out[4],in0[4],in1[4]);
+or(out[3],in0[3],in1[3]);
+or(out[2],in0[2],in1[2]);
+or(out[1],in0[1],in1[1]);
+or(out[0],in0[0],in1[0]);
 //simulating the circuit
 initial begin
-in = 16'b0000000000000000;
+in0 = 16'b1111111111111111;
+in1 = 16'b1000000000011111;
 #1;
-$display("Input: ",in," Output: ",out);
+$display("Input 0: ",in0," Input 1: ",in1," Output: ", out);
 $finish;
 end
 endmodule
