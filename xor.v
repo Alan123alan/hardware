caefac61e68245236438834e4816a09f7238433b
Alan123alan <alancastellanos2@hotmail.com>
2023-06-20 15:44:34 -0600

    Added port list to Xor module and moved simulation to test bench file

--- xor.v
+++ xor.v
@@ -1,22 +1,11 @@
-module test_module;
-//an XOR gate can be implemented as (NOT(A) AND B) OR (NOT(B) AND A)
-//declaring inputs and outputs
-reg A;
-reg B;
-wire notA,notB,notAandB,notBandA,notAandBornotBandA;
+module Xor(input i0, input i1, output out);
+//an XOR gate can be implemented as (NOT(i0) AND i1) OR (NOT(i0) AND i1)
+//declaring temps 
+wire not_i0,not_i1,not_i0_and_i1,not_i1_and_i0;
 //modelling the circuit
-not(notA,A);
-not(notB,B);
-and(notAandB,notA,B);
-and(notBandA,notB,A);
-or(notAandBornotBandA,notAandB,notBandA);
-//simulating the circuit
-initial
-begin
-A=1'b0;
-B=1'b1;
-#1;
-$display("Input A: ",A," Input B: ",B," XOR Output: ",notAandBornotBandA);
-$finish;
-end
+not(not_i0,i0);
+not(not_i1,i1);
+and(not_i0_and_i1,not_i0,i1);
+and(not_i1_and_i0,not_i1,i0);
+or(out,not_i0_and_i1,not_i1_and_i0);
 endmodule
