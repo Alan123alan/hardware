85b82ded08504a7535751d2ea9e211e89f5dc8f2
Alan123alan <alancastellanos2@hotmail.com>
2023-07-02 22:40:48 -0700

    implemented set reset latch circuit

--- memory/SRLatch.v
+++ memory/SRLatch.v
@@ -1,2 +1,11 @@
-module SRLatch();
+`include "../gates/Nor.v"
+module SRLatch(output t0, output t1, input enable, input reset, input set);
+//declaring temps
+wire enable_and_set;
+wire enable_and_reset;
+//modelling the circuit
+and(enable_and_reset,enable,reset);
+and(enable_and_set,enable,set);
+Nor Nor_t1(t0,enable_and_reset,t1);
+Nor Nor_t0(t1,enable_and_set,t0);
 endmodule
